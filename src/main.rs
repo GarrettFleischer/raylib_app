d0a5ce13a2b6bd1bf1873e26607a54903bf7b964
Garrett Fleischer <garrett.fleischer@yahoo.com>
2022-08-14 19:50:31 -0700

    state with frame count

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use raylib::consts::KeyboardKey::*;
 use raylib::consts::MouseButton::*;
+use raylib::ffi::GetRandomValue;
 use raylib::prelude::*;
 
 const SCREEN_WIDTH: f32 = 640.0;
@@ -13,42 +14,57 @@ struct Ball {
     color: Color,
 }
 
+struct State {
+    frame: i32,
+    value: i32,
+    ball: Ball,
+}
+
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32)
-        .title("Keyboard Input")
+        .title("Raylib App")
         .vsync()
         .build();
 
-    let mut ball = Ball {
-        position: Vector2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
-        speed: 3.0,
-        radius: 40.0,
-        color: Color::GREEN,
+    let mut state = State {
+        ball: Ball {
+            position: Vector2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
+            speed: 3.0,
+            radius: 40.0,
+            color: Color::GREEN,
+        },
+        frame: 0,
+        value: get_random_value(-100, 100),
     };
 
     while !rl.window_should_close() {
-        update(&rl, &mut ball);
+        update(&rl, &mut state);
 
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
-        draw(&mut d, &ball);
+        draw(&mut d, &state);
     }
 }
 
-fn update(rl: &RaylibHandle, ball: &mut Ball) {
+fn update(rl: &RaylibHandle, state: &mut State) {
+    state.frame = (state.frame + 1) % 60;
+    if state.frame == 0 {
+        state.value = get_random_value(-100, 100);
+    }
+
     if rl.is_key_down(KEY_RIGHT) {
-        ball.position.x += ball.speed;
+        state.ball.position.x += state.ball.speed;
     } else if rl.is_key_down(KEY_LEFT) {
-        ball.position.x -= ball.speed;
+        state.ball.position.x -= state.ball.speed;
     } else if rl.is_key_down(KEY_DOWN) {
-        ball.position.y += ball.speed;
+        state.ball.position.y += state.ball.speed;
     } else if rl.is_key_down(KEY_UP) {
-        ball.position.y -= ball.speed;
+        state.ball.position.y -= state.ball.speed;
     }
 
     if rl.is_key_pressed(KEY_SPACE) {
-        ball.color = if ball.color == Color::GREEN {
+        state.ball.color = if state.ball.color == Color::GREEN {
             Color::YELLOW
         } else {
             Color::GREEN
@@ -56,11 +72,11 @@ fn update(rl: &RaylibHandle, ball: &mut Ball) {
     }
 
     if rl.is_mouse_button_down(MOUSE_LEFT_BUTTON) {
-        ball.position = ball.position.lerp(rl.get_mouse_position(), 0.1)
+        state.ball.position = state.ball.position.lerp(rl.get_mouse_position(), 0.1)
     }
 
     if rl.is_mouse_button_pressed(MOUSE_RIGHT_BUTTON) {
-        ball.color = if ball.color == Color::GREEN {
+        state.ball.color = if state.ball.color == Color::GREEN {
             Color::RED
         } else {
             Color::GREEN
@@ -68,6 +84,18 @@ fn update(rl: &RaylibHandle, ball: &mut Ball) {
     }
 }
 
-fn draw(d: &mut RaylibDrawHandle, ball: &Ball) {
-    d.draw_circle_v(ball.position, ball.radius, ball.color);
+fn draw(d: &mut RaylibDrawHandle, state: &State) {
+    draw_text_center(d, format!("{}", state.value).as_str(), 10, 32, Color::WHITE);
+    d.draw_circle_v(state.ball.position, state.ball.radius, state.ball.color);
+}
+
+fn draw_text_center(d: &mut RaylibDrawHandle, text: &str, y: i32, font_size: i32, color: Color) {
+    let text_left = measure_text(text, font_size) / 2;
+    d.draw_text(
+        text,
+        (SCREEN_WIDTH as i32) / 2 - text_left,
+        y,
+        font_size,
+        color,
+    )
 }
